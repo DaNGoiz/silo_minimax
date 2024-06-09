c2d32a2d123f9b802a80fa194a2b058d8f492e59
Izzy <3332099165@qq.com>
2024-06-09 18:47:40 +0800

    Update minimax_classic.py

--- minimax_classic/minimax_classic.py
+++ minimax_classic/minimax_classic.py
@@ -6,41 +6,35 @@ class Game:
         self.silos = silos
         self.state = initial_state if initial_state else '.' * (self.silos * 3)
         self.evaluation_cache = {}
-        self.count = 0
+        self.count = 0 # debug
 
     def get_moves(self):
-        moves = []
-        for i in range(self.silos):
-            if '.' in self.state[i*3:i*3+3]:
-                moves.append((i, 'red'))
-                moves.append((i, 'blue'))
-        return moves
+        red_moves = [(i, 'red') for i in range(self.silos) if self.state[i*3:i*3+3].count('.') > 0]
+        blue_moves = [(i, 'blue') for i in range(self.silos) if self.state[i*3:i*3+3].count('.') > 0]
+        return red_moves + blue_moves
 
     def make_move(self, team, move):
         pos = self.state[move*3:move*3+3].index('.')
         new_state = list(self.state)
-        new_state[move*3 + pos] = 'R' if team == 'red' else 'B'
+        new_state[move*3+pos] = 'R' if team == 'red' else 'B'
         return ''.join(new_state)
 
-    def unmake_move(self, original_state):
+    def unmake_move(self, move, original_state):
         self.state = original_state
 
     def is_terminal(self):
         if '.' not in self.state:
             return True
         for team_char in ['R', 'B']:
-            for i in range(self.silos):
-                sub = self.state[i*3:i*3+3]
-                if sub.count(team_char) == 2 and sub[0] == team_char:
-                    if self.state.count(team_char) >= 3:
-                        return True
+            if sum(sub.count(team_char) == 2 and sub[0] == team_char for sub in [self.state[i*3:i*3+3] for i in range(self.silos)]) >= 3:
+                return True
         return False
 
     def evaluate(self, team):
         score = self.state.count('R') * 30 if team == 'red' else self.state.count('B') * 30
         return score
 
-    def minimax(self, depth, team, parent_state=None, decision_map=None):
+    def minimax(self, depth, team, parent_state=None, move_name=None, decision_map=None):
         if decision_map is None:
             decision_map = {}
 
@@ -58,17 +52,19 @@ class Game:
             original_state = self.state
             self.state = self.make_move(move_team, move)
  
-            value = self.minimax(depth - 1, team, self.state, decision_map)
-            self.unmake_move(original_state)
+            value = self.minimax(depth - 1, team, self.state, move_name, decision_map)
+            self.unmake_move(move, original_state)
 
-            if (team == 'red' and value > best_value) or (team == 'blue' and value < best_value):
+            if team == 'red' and value > best_value or team == 'blue' and value < best_value:
                 best_value = value
                 decision_map[parent_state] = [move]
             elif value == best_value:
                 decision_map[parent_state].append(move)
             
+        
         self.evaluation_cache[cache_key] = best_value
 
+        # debug
         self.count += 1
         print(f"No: {self.count}, State: {parent_state}, Best moves: {decision_map[parent_state] if parent_state in decision_map else None}, Best Value: {best_value}")
 
